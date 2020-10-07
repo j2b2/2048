ca1073a09b1c80f97917272e0c2cded1d796ac21
j2b2 <jean.betrema@gmail.com>
2020-10-07 15:24:51 +0200

    Restore after git bug

--- play2048.jl
+++ play2048.jl
@@ -1,23 +1,32 @@
 using jb2048
-# julia set working module -> jb2048
+# Alt-m julia set working module
 g=initgame()
 initplot()
 play!(g, display=true)
 play!(g, display=true, target=(10,2))
+play!(g, display=true, moves=150)
 play!(g, display=true, moves=1)
 
-i = 0
+staticeval(g.board)
+[maxeval(g.board,k).score for k in 1:5]
+
+empty!(staticache);empty!(meancache)
+
+length(staticache), length(meancache)
+
+i = -16
 i -= 1;plot(g.hist[i]...)   # "splat" argument (aplati, écrasé)
 i += 1;plot(g.hist[i]...)
 e=evals(g.hist[i][1],3)
 e[2:4,2:3]
 evals(g.board,2)
+g.hist[i]
+back!(g,64)
 
-b=[12 11 6 3; 9 8 6 2; 5 2 0 1; 0 0 1 2]
+b,move=[13 10 6 2; 9 7 4 1; 3 3 4 1; 2 0 0 0], 4529
 g=Game(b)
-g.move = round(sum(1 << k for k in b) / 2.2)
+g.move = move
 plot(g)
-setcareful(3, 4)
 setgamma()
 
 back!(g,-1);plot(g)
@@ -31,7 +40,7 @@ g.motion
 @time m,r,hg = repartition(10)
 g=hg[9]
 
-setcareful(4, 5)
+setcareful(3,6,3,100)
 g=Game([13 11 9 5; 12 10 6 3; 3 4 3 2; 2 2 2 0])
 @time m,r,hg = repartition(2,game=g,target=(14,1))
 maximum(g.move for g in hg)
